883b904615cf91dc1a55dcc0f4f9a1814aaee0ec
liaochenliang <liaochenliang@agora.io>
2026-03-12 14:30:37 +0800

    #Add - KTVApiImpl

--- iOS/AgoraEntScenarios/Common/KTVApi/KTVApiImpl.swift
+++ iOS/AgoraEntScenarios/Common/KTVApi/KTVApiImpl.swift
@@ -156,9 +156,13 @@ private func agoraPrint(_ message: String) {
         engine.setParameters("{\"che.audio.direct.uplink_process\": false}")
         engine.setParameters("{\"che.audio.neteq.enable_stable_playout\":true}")
         engine.setParameters("{\"che.audio.neteq.targetlevel_offset\": 20}")
+        engine.setParameters("{\"che.audio.aec.split_srate_for_48k\": 16000}")
         engine.setParameters("{\"che.audio.ans.noise_gate\": 20}")
+        engine.setParameters("{\"rtc.use_audio4\": true}")
         if apiConfig?.type == .singRelay {
             engine.setParameters("{\"che.audio.aiaec.working_mode\": 1}")
+        } else {
+            engine.setParameters("{\"che.audio.aiaec.working_mode\": 0}")
         }
     }
     
@@ -567,6 +571,7 @@ extension KTVApiImpl {
             apiConfig?.engine?.updateChannel(with: mediaOption)
 
             mediaPlayer?.setPlayerOption("enable_multi_audio_track", value: 0)
+            mediaPlayer?.setPlayerOption("play_pos_change_callback", value: 100)
             if apiConfig?.musicType == .mcc {
                 (mediaPlayer as? AgoraMusicPlayerProtocol)?.openMedia(songCode: self.songCode , startPos: 0)
             } else {
@@ -807,6 +812,7 @@ extension KTVApiImpl {
         if self.singerRole == .leadSinger || self.singerRole == .soloSinger {
             mediaPlayer?.setPlayerOption("enable_multi_audio_track", value: 1)
         }
+        mediaPlayer?.setPlayerOption("play_pos_change_callback", value: 100)
         apiConfig?.engine?.adjustPlaybackSignalVolume(Int(remoteVolume))
         let ret = (mediaPlayer as? AgoraMusicPlayerProtocol)?.openMedia(songCode: songCode, startPos: startPos)
         agoraPrint("startSing->openMedia(\(songCode) fail: \(ret ?? -1)")
@@ -821,6 +827,7 @@ extension KTVApiImpl {
             return
         }
         apiConfig?.engine?.adjustPlaybackSignalVolume(Int(remoteVolume))
+        mediaPlayer?.setPlayerOption("play_pos_change_callback", value: 100)
         let ret = mediaPlayer?.open(url, startPos: 0)
         agoraPrint("startSing->openMedia(\(url) fail: \(ret ?? -1)")
     }
@@ -1006,14 +1013,15 @@ extension KTVApiImpl {
         if mediaPlayer?.getPlayerState() == .playing {
             let localNtpTime = getNtpTimeInMs()
             let localPosition = localNtpTime - Int(localPlayerSystemTime) + localPosition
-            let expectPosition = Int(dict["time"] as? Int64 ?? 0) + localNtpTime - Int(dict["ntp"] as? Int64 ?? 0) + self.audioPlayoutDelay
+            var expectPosition = Int(dict["time"] as? Int64 ?? 0) + localNtpTime - Int(dict["ntp"] as? Int64 ?? 0) + self.audioPlayoutDelay
             let threshold = expectPosition - Int(localPosition)
             let ntpTime = dict["ntp"] as? Int ?? 0
             let time = dict["time"] as? Int64 ?? 0
-            agoraPrint("checkNtp, diff:\(threshold), localNtp:\(getNtpTimeInMs()), localPosition:\(localPosition), audioPlayoutDelay:\(audioPlayoutDelay), remoteDiff:\(String(describing: ntpTime - Int(time)))")
+           // agoraPrint("checkNtp, diff:\(threshold), localNtp:\(getNtpTimeInMs()), localPosition:\(localPosition), audioPlayoutDelay:\(audioPlayoutDelay), remoteDiff:\(String(describing: ntpTime - Int(time)))")
             if abs(threshold) > 50 {
-                print("expectPosition:\(expectPosition)")
-                 mediaPlayer?.seek(toPosition: expectPosition)
+                 expectPosition += mediaPlayer?.getAudioBufferDelay() ?? 0
+                  agoraPrint("expectPosition:\(expectPosition)")
+                  mediaPlayer?.seek(toPosition: expectPosition)
             }
         }
         
